9fd7a3c656bd3ccf5f43c48e76c2d3e3913ec5cd
dennismacc <dennis.maccarthy@gmail.com>
2022-08-26 16:19:33 -0700

    update models

---
+++ models/comment.js
@@ -0,0 +1,49 @@
+//Import sequelize library/package
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//Defines 'Comment' as a model
+class Comment extends Model{}
+
+//Initialize Comment Model
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    } 
+  
+  },
+
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,13 +1,36 @@
-const User = require('./User');
-const Project = require('./Project');
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+  });
+  
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+  });
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+  });
 
-module.exports = { User, Project };
+module.exports = {User, Post, Comment}
