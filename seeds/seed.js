71e6b922fb287bfabbe3eaf3737a15d279e2b0c9
dennismacc <dennis.maccarthy@gmail.com>
2022-08-26 17:41:48 -0700

    updated and created new seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,25 +1,15 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
-
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
-
-  process.exit(0);
+  await User.bulkCreate(userData, { individualHooks: true });
+  await Post.bulkCreate(postData);
+  await Comment.bulkCreate(commentData);
 };
 
 seedDatabase();
