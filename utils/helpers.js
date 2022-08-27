278c75276d34446d25b9cbcfbb508818a2912545
dennismacc <dennis.maccarthy@gmail.com>
2022-08-26 23:15:43 -0700

    add handlebars. testing app. login/signup not working

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,22 +1,25 @@
-module.exports = {
-  format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
-  },
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
-  },
-  get_emoji: () => {
-    const randomNum = Math.random();
+const globals = {};
 
-    // Return a random emoji
-    if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
-    } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
-    } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
-    }
-  },
+module.exports = {
+    formatDate: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+        date
+        ).getFullYear()}`;
+    },
+    isLocalUser: (currentUserId, userId) => {
+        console.log("Is current user --- ", currentUserId, userId)
+        return currentUserId === userId
+    },
+    print: data => console.log('from helper: ', data),
+    setGlobalVar: (key, value) => {
+        globals[key] = value;
+        console.log('set global key: ', key, ' to value: ', value);
+        
+    },
+    getGlobalVar: key => {
+        const value = globals[key];
+        console.log('got global key: ', key, ' who\'s value is: ', value);
+        return value;
+    },
+    notEmpty: arr => arr.length > 0
 };
